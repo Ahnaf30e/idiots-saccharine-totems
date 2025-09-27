d82fae1f145873f44e4b5c3b7e6982370a54182c
Ahnaf30e <auladhossain38@gmail.com>
2025-09-27 22:54:51 +0600

    Orgnaize

---
+++ src/main/java/dev/ahnaf30eidiot/item/ModItems.java
@@ -0,0 +1,11 @@
+package dev.ahnaf30eidiot.item;
+
+import dev.ahnaf30eidiot.tok.TotemOfKeeping;
+
+public class ModItems {
+
+    
+    public static void registerModItems() {
+        TotemOfKeeping.LOGGER.info("Registering mod items for " + TotemOfKeeping.MOD_ID);
+    }
+}
