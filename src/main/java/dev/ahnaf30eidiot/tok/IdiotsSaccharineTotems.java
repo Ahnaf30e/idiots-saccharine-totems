f11d519e70dbdd9a2536a32b737b4fa3d0e51711
Ahnaf30e <auladhossain38@gmail.com>
2025-10-31 00:35:05 +0600

    Logger uses IdiotsSaccharineTotems now

--- src/main/java/dev/ahnaf30eidiot/tok/IdiotsSaccharineTotems.java
+++ src/main/java/dev/ahnaf30eidiot/tok/IdiotsSaccharineTotems.java
@@ -20,7 +20,7 @@ public class IdiotsSaccharineTotems implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+	public static final Logger LOGGER = LoggerFactory.getLogger("IdiotsSaccharineTotems");
 
 	@Override
 	public void onInitialize() {
